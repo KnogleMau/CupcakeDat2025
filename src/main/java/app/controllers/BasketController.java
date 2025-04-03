508d263a488cfc9e69d0dfdbfdebc3e4378c9577
KnogleMau <mav.solver@hotmail.com>
2025-04-03 11:47:36 +0200

    View Basket

---
+++ src/main/java/app/controllers/BasketController.java
@@ -0,0 +1,18 @@
+package app.controllers;
+
+import app.entities.Cupcake;
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BasketController {
+
+    public static void BasketRoutes(Javalin app, ConnectionPool connectionPool){
+
+
+    }
+
+}
